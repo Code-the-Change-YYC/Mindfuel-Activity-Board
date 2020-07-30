015c7e6d7c69b77eeb5cb521b44478be7b459483
evan-krul <evan@krul.ca>
2020-07-29 18:39:28 -0600

    * FIxed docker container by adding a temporary dev setting to run npm install before starting the server. * Added something for code completion in VS code. * Fixed case issues with capitolization

--- frontend_react/src/App.tsx
+++ frontend_react/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Home from './views/home/Home';
+import Home from './views/Home/Home';
 
 function App() {
   return (
