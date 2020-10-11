ae33d11835df7dfed1ed0d2d938b012ea1c82878
Arsalan Fardi <arsalan.fardi@gmail.com>
2020-10-11 13:01:04 -0600

    Implement fullscreen Map component on home page

--- frontend_react/src/App.tsx
+++ frontend_react/src/App.tsx
@@ -6,21 +6,7 @@ import Home from './views/Home/Home';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          The React APP
-        </a>
         <Home name="Mindfuel team"></Home>
-      </header>
     </div>
   );
 }
--- frontend_react/src/components/Map/MapComponent.tsx
+++ frontend_react/src/components/Map/MapComponent.tsx
@@ -1,2 +1,21 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import GoogleMapReact from 'google-map-react';
+import styles from './Map.module.css';
+
+const Map = (props: any) => {
+    // Default center of North America
+    const [center, setCenter] = useState({lat: 48.354594, lng: -99.998050 });
+    const [zoom, setZoom] = useState(4);
+    return (
+      <div className="map" style={{ height: '100vh', width: '100vw' }}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: `${[process.env.REACT_APP_APIKEY]}` }}
+          defaultCenter={center}
+          defaultZoom={zoom}
+        >
+        </GoogleMapReact>
+      </div>
+    );
+}
+
+export default Map;
