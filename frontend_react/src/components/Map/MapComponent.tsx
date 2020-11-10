8f57cd7d3f98f35ac1bd30f80cc078b3367a6580
Arsalan Fardi <arsalan.fardi@gmail.com>
2020-11-09 20:15:35 -0700

    Clean home component and update style usage

--- frontend_react/src/components/Map/MapComponent.tsx
+++ frontend_react/src/components/Map/MapComponent.tsx
@@ -2,12 +2,12 @@ import React, { useState } from 'react';
 import GoogleMapReact from 'google-map-react';
 import styles from './Map.module.css';
 
-const Map = (props: any) => {
+const Map = () => {
     // Default center of North America
-    const [center, setCenter] = useState({lat: 48.354594, lng: -99.998050 });
+    const [center, setCenter] = useState({ lat: 48.354594, lng: -99.998050 });
     const [zoom, setZoom] = useState(4);
     return (
-      <div className="map" style={{ height: '100vh', width: '100vw' }}>
+      <div className={styles.map}>
         <GoogleMapReact
           bootstrapURLKeys= {{key: `${[process.env.REACT_APP_GOOGLE_MAPS_API_KEY]}`}}
           defaultCenter={center}
--- frontend_react/src/views/Home/Home.tsx
+++ frontend_react/src/views/Home/Home.tsx
@@ -9,19 +9,6 @@ interface HomeProps {
 const Home: React.FunctionComponent<HomeProps> = (props) => {
     return (
       <Map></Map>
-        // <div>
-        //     <h4>Hello, {props.name}</h4>
-        //     <div className={styles.homeInfo} >
-        //     <li><b>src/api -</b> holds all the functions related to API calls and its helpers </li>
-        //     <li><b>src/components -</b> Shared components that are being used in the app more than once or are not on a screen </li>
-        //     <li><b>src/res -</b> Resource path where we can place all our assets, colors, constants, dimens, env-config, themes and so on</li>
-        //     <li><b>src/store -</b> Redux data that is shared in most or all screens (optional - depending on whether we have redux pattern)</li>
-        //     <li><b>src/utils -</b> Helpers and utils that are used in multiple screens and components</li>
-        //     <li><b>src/views -</b> All important screens/views in the project, each screen/view can have their own tests, hooks, store paths related to that the screen/view mainly, as well as, their own routes.ts, helpers.ts and styles.ts files. The idea here is having multiple sections under specific screens like tabs or steps (if applicable). If those tabs/steps have its own "sections", they may be a good fit to have its own space under views path. That way, we can keep the structure organised and prevent too deep and complex nested hierarchy</li>
-
-        //     <p><b>Note: </b> application uses TypeScript and CSS modules. For demo, I have created home.tsx which uses TypeScript and home.module.css as CSS modules!</p>
-        //     </div>
-        // </div>
     );
 }
 
