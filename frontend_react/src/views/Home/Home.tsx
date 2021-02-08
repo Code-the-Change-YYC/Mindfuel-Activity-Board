76e2f2cf7bb842bda947b79c8d03dc22c9b1a643
Arsalan Fardi <arsalan.fardi@gmail.com>
2021-02-07 20:16:54 -0700

    Create sidenav on Home page view

--- frontend_react/src/views/Home/Home.tsx
+++ frontend_react/src/views/Home/Home.tsx
@@ -5,47 +5,54 @@ import Timeline from '../../components/Timeline/TimelineComponent'
 import styles from './Home.module.css';
 
 interface HomeProps {
-    name: string,
+  name: string,
 }
 
 const data = [
-    {
-        "numberValue": "56",
-        "textValue": "Total Users",
-        "icon": require("../../assets/users.svg")
-    },
-    {
-        "numberValue": "3",
-        "textValue": "Countries",
-        "icon": require("../../assets/flag.svg")
-    },
-    {
-        "numberValue": "16",
-        "textValue": "Cities",
-        "icon": require("../../assets/location.svg")
-    }
+  {
+    "numberValue": "56",
+    "textValue": "Total Users",
+    "icon": require("../../assets/users.svg")
+  },
+  {
+    "numberValue": "3",
+    "textValue": "Countries",
+    "icon": require("../../assets/flag.svg")
+  },
+  {
+    "numberValue": "16",
+    "textValue": "Cities",
+    "icon": require("../../assets/location.svg")
+  }
 ]
 
-const Home: React.FunctionComponent<HomeProps> = (props) => {
-    const boxes = data.map(box => {
-        return <AnalyticsBox numberValue={box.numberValue} textValue={box.textValue} icon={box.icon}></AnalyticsBox>
-    })
+const logo = require("../../assets/mindfuel-logo.png")
 
-    return (
-      <React.Fragment>
+const Home: React.FunctionComponent<HomeProps> = (props) => {
+  const boxes = data.map(box => {
+    return <AnalyticsBox numberValue={box.numberValue} textValue={box.textValue} icon={box.icon}></AnalyticsBox>
+  })
+
+  return (
+    <React.Fragment>
+      <div className={styles.sideNav}>
+        <div className={styles.logoContainer}>
+          <img className={styles.logo} src={logo} />
+        </div>
         <div className={styles.analyticboxes}>
-            { boxes }
+          {boxes}
         </div>
-        <div className={styles.map}>
-          <Map></Map>
-        </div>
+      </div>
+      <div className={styles.map}>
+        <Map></Map>
         <div className={styles.timelineContainer}>
           <div className={styles.timeline}>
             <Timeline></Timeline>
           </div>
         </div>
-      </React.Fragment>
-    );
+      </div>
+    </React.Fragment>
+  );
 }
 
 export default Home;
