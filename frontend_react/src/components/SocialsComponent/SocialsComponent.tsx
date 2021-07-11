09ba6488615682119460f550fdb79ec008d9c98c
Arsalan Fardi <arsalan.fardi@gmail.com>
2021-07-11 17:24:50 -0600

    Implement react-twitter-embed component as a popover in top right handside

--- frontend_react/src/components/SocialsComponent/SocialsComponent.tsx
+++ frontend_react/src/components/SocialsComponent/SocialsComponent.tsx
@@ -4,17 +4,21 @@ import Button from "@material-ui/core/Button";
 import styles from "./SocialsComponent.module.css";
 import IconButton from "@material-ui/core/IconButton";
 import TwitterIcon from "@material-ui/icons/Twitter";
-// import { TwitterTimelineEmbed } from "react-twitter-embed";
+import { TwitterTimelineEmbed } from "react-twitter-embed";
 
-const TwitterTimelineEmbed = require("react-twitter-embed");
+const twitterLink =
+  "https://twitter.com/MindFuelca?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor";
+
 const SocialsComponent = () => {
-  const [anchorEl, setAnchorEl] = React.useState<HTMLImageElement | null>(null);
+  const [anchorEl, setAnchorEl] =
+    React.useState<HTMLButtonElement | null>(null);
   const iconClasses = {
     root: styles.socialsIconButton,
   };
-  //   const handleClick = (event: React.MouseEvent<HTMLImageElement>) => {
-  //     setAnchorEl(event.currentTarget);
-  //   };
+
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
 
   const handleClose = () => {
     setAnchorEl(null);
@@ -28,29 +32,37 @@ const SocialsComponent = () => {
       <IconButton
         aria-label="open drawer"
         color="inherit"
-        // onClick={handleDrawerToggle}
+        onClick={handleClick}
         classes={iconClasses}
       >
         <TwitterIcon style={{ fontSize: 30 }} />
       </IconButton>
       <Popover
-        // id={id}
+        id={id}
         open={open}
         anchorEl={anchorEl}
-        // onClose={handleClose}
+        onClose={handleClose}
         anchorOrigin={{
           vertical: "bottom",
-          horizontal: "center",
+          horizontal: "left",
         }}
         transformOrigin={{
           vertical: "top",
           horizontal: "center",
         }}
       >
+        <div className={styles.popoverHeader}>
+          Tweets by&nbsp;
+          <a href={twitterLink} target="_blank">
+            @MindFuelca
+          </a>
+        </div>
         <TwitterTimelineEmbed
           sourceType="profile"
           screenName="mindfuelca"
-          options={{ height: 400 }}
+          options={{ height: 400, width: 275 }}
+          noFooter={true}
+          noHeader={true}
         />
       </Popover>
     </React.Fragment>
