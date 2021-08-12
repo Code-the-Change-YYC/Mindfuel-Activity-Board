c44040c620ff7a156fca838ce72de01ac917797e
Arsalan Fardi <arsalan.fardi@gmail.com>
2021-08-12 01:03:04 -0600

    Implement connection to MindFuel websocket, looping TBC

--- rest_golang/main.go
+++ rest_golang/main.go
@@ -1,100 +1,47 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"github.com/gorilla/mux"
-	"io/ioutil"
+	"flag"
+	"github.com/gorilla/websocket"
 	"log"
-	"net/http"
+	"net/url"
+	"os"
+	"os/signal"
 )
 
-type event struct {
-	ID          string `json:"ID"`
-	Title       string `json:"Title"`
-	Description string `json:"Description"`
-}
+var done chan interface{}
+var interrupt chan os.Signal
+var addr = flag.String("addr", "wonderville.org:5556", "wss service address")
 
-type allEvents []event
-
-var events = allEvents{
-	{
-		ID:          "1",
-		Title:       "Introduction to Golang",
-		Description: "Come join us for a chance to learn how golang works and get to eventually try it out",
-	},
-}
-
-func homeLink(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome home!")
-}
-
-func createEvent(w http.ResponseWriter, r *http.Request) {
-	var newEvent event
-	reqBody, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		fmt.Fprintf(w, "Kindly enter data with the event title and description only in order to update")
-	}
-
-	json.Unmarshal(reqBody, &newEvent)
-	events = append(events, newEvent)
-	w.WriteHeader(http.StatusCreated)
-
-	json.NewEncoder(w).Encode(newEvent)
-}
-
-func getOneEvent(w http.ResponseWriter, r *http.Request) {
-	eventID := mux.Vars(r)["id"]
-
-	for _, singleEvent := range events {
-		if singleEvent.ID == eventID {
-			json.NewEncoder(w).Encode(singleEvent)
-		}
-	}
-}
-
-func getAllEvents(w http.ResponseWriter, r *http.Request) {
-	json.NewEncoder(w).Encode(events)
-}
-
-func updateEvent(w http.ResponseWriter, r *http.Request) {
-	eventID := mux.Vars(r)["id"]
-	var updatedEvent event
-
-	reqBody, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		fmt.Fprintf(w, "Kindly enter data with the event title and description only in order to update")
-	}
-	json.Unmarshal(reqBody, &updatedEvent)
-
-	for i, singleEvent := range events {
-		if singleEvent.ID == eventID {
-			singleEvent.Title = updatedEvent.Title
-			singleEvent.Description = updatedEvent.Description
-			events = append(events[:i], singleEvent)
-			json.NewEncoder(w).Encode(singleEvent)
-		}
-	}
-}
-
-func deleteEvent(w http.ResponseWriter, r *http.Request) {
-	eventID := mux.Vars(r)["id"]
-
-	for i, singleEvent := range events {
-		if singleEvent.ID == eventID {
-			events = append(events[:i], events[i+1:]...)
-			fmt.Fprintf(w, "The event with ID %v has been deleted successfully", eventID)
-		}
-	}
+func receiveHandler(connection *websocket.Conn) {
+    defer close(done)
+    for {
+        _, msg, err := connection.ReadMessage()
+        if err != nil {
+            log.Println("Error in receive:", err)
+            return
+        }
+        log.Printf("Received: %s\n", msg)
+    }
 }
 
 func main() {
-	router := mux.NewRouter().StrictSlash(true)
-	router.HandleFunc("/", homeLink)
-	router.HandleFunc("/event", createEvent).Methods("POST")
-	router.HandleFunc("/events", getAllEvents).Methods("GET")
-	router.HandleFunc("/events/{id}", getOneEvent).Methods("GET")
-	router.HandleFunc("/events/{id}", updateEvent).Methods("PATCH")
-	router.HandleFunc("/events/{id}", deleteEvent).Methods("DELETE")
-	log.Fatal(http.ListenAndServe(":8080", router))
+	// TODO: Remove flags and migrate to env
+	flag.Parse()
+	log.SetFlags(0)
+
+    done = make(chan interface{}) // Channel to indicate that the receiverHandler is done
+    interrupt = make(chan os.Signal) // Channel to listen for interrupt signal to terminate gracefully
+ 
+    signal.Notify(interrupt, os.Interrupt) // Notify the interrupt channel for SIGINT
+	
+	socketUrl := url.URL{Scheme: "wss", Host: *addr}
+	log.Printf("Connecting to %s",socketUrl.String())
+
+    conn, _, err := websocket.DefaultDialer.Dial(socketUrl.String(), nil)
+    if err != nil {
+        log.Fatal("Error connecting to Websocket Server:", err)
+    }
+    defer conn.Close()
+    go receiveHandler(conn)
 }
