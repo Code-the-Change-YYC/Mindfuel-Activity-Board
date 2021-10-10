7b83fd99fcfbf8ae6eb082f48167ffe7c9e16013
Ramin Radmand <69999501+imraminradmand@users.noreply.github.com>
2021-10-09 19:18:27 -0600

    Setup most of elastic writer, needs connection to socket

--- rest_golang/elastic/ElasticWriter.go
+++ rest_golang/elastic/ElasticWriter.go
@@ -1,25 +1,62 @@
 package elastic
 
 import (
-	"log"
-	"github.com/elastic/go-elasticsearch/v7"
+	"context"
+	"encoding/json"
+	"fmt"
+
+	elastic "gopkg.in/olivere/elastic.v7"
 )
 
-func saveUsertoElastic(jsonBytes []byte) {
-	// TODO: use env file for this
-	addresses := []string{"http://localhost:9200"}
-	cfg := elasticsearch.Config{Addresses: addresses}
-	client, err := elasticsearch.NewClient(cfg)
-	if err != nil {
-		log.Println("elastic client failed to connect:", err)
-		return
-	}
+func GetElasticClient() (*elastic.Client, error) {
+	client, err := elastic.NewClient(elastic.SetURL("http://localhost:9200"),
+		elastic.SetSniff(false),
+		elastic.SetHealthCheck(false))
 
-	log.Println(client)
+	fmt.Println("Elasticsearch initialized...")
 
-	// Check if a "mindfuel_activity" index exists
-	// Create a "mindfuel_activity" index if it doesn't 
-	// Write index and user payload to database
-	// Look into geopoint type for storing lat/long pairs
+	return client, err
 }
 
+func saveUsertoElastic() {
+	ctx := context.Background()
+	esclient, err := GetElasticClient()
+
+	if err != nil {
+		fmt.Println("Elastic init fail : ", err)
+		panic("client failed")
+	}
+
+	//we need to get json from the socket and then this should write it
+	//it will replace the something that is in the bracket
+	dataJSON, err := json.Marshal("something")
+	js := string(dataJSON)
+	ind, err := esclient.Index().
+		Index("wondervilleasset").
+		BodyJson(js).
+		Do(ctx)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("inserstion successful")
+}
+
+// func saveUsertoElastic(jsonBytes []byte) {
+// 	// TODO: use env file for this
+// 	addresses := []string{"http://localhost:9200"}
+// 	cfg := elasticsearch.Config{Addresses: addresses}
+// 	client, err := elasticsearch.NewClient(cfg)
+// 	if err != nil {
+// 		log.Println("elastic client failed to connect:", err)
+// 		return
+// 	}
+
+// 	log.Println(client)
+
+// 	// Check if a "mindfuel_activity" index exists
+// 	// Create a "mindfuel_activity" index if it doesn't
+// 	// Write index and user payload to database
+// 	// Look into geopoint type for storing lat/long pairs
+// }
