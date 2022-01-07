477081a65fa0291f12bf37f0bb68d8009d384466
parsahonarmand <parsa.honarmand@gmail.com>
2022-01-07 00:53:51 -0700

    Structs needed to parse socket messages

--- rest_golang/model/WondervilleAssetModel.go
+++ rest_golang/model/WondervilleAssetModel.go
@@ -1,26 +1,54 @@
 package model
 
-import "time"
+type AssetMessage struct {
+	Type    string       `json:"type"`
+	Payload AssetPayload `json:"payload"`
+}
 
-/*
- For   "type": "wondervilleAsset",
-// TODO do we split the sub-structs?
-*/
-type WondervilleAsset struct {
-	DateTime time.Time
+type SessionMessage struct {
+	Type    string         `json:"type"`
+	Payload SessionPayload `json:"payload"`
+}
+
+type AssetPayload struct {
 	Ip       string   `json:"ip"`
 	Url      string   `json:"url"`
 	Location Location `json:"location"`
 	Asset    Asset    `json:"asset"`
+	Stats    Stats    `json:"stats"`
 	Rank     int      `json:"rank"`
 }
 
+type SessionPayload struct {
+	Location Location `json:"location"`
+}
+
+type Stats struct {
+	Top     TopStats   `json:"stats"`
+	Summary []StatType `json:"statsSummary"`
+}
+
+type TopStats struct {
+	Game     StatType `json:"Game"`
+	Video    StatType `json:"Video"`
+	Activity StatType `json:"Activity"`
+	Story    StatType `json:"Story"`
+}
+
+type StatType struct {
+	Type   string `json:"type"`
+	Hits   int    `json:"hits"`
+	Top    string `json:"top"`
+	TopUrl string `json:"topUrl"`
+	Rank   int    `json:"rank"`
+}
+
 type Location struct {
-	Country string  `json:"country"`
-	Region  string  `json:"region_name"`
-	City        string  `json:"city"`
-	Longitude   float32 `json:"longitude"`
-	Latitude    float32 `json:"latitude"`
+	Country   string  `json:"country_name"`
+	Region    string  `json:"region_name"`
+	City      string  `json:"city"`
+	Longitude float32 `json:"longitude"`
+	Latitude  float32 `json:"latitude"`
 }
 
 type Asset struct {
