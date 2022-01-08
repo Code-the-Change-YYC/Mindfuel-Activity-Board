d32b836e770dbf2774b390eae57556c4de840632
Ramin Radmand <69999501+imraminradmand@users.noreply.github.com>
2022-01-07 22:27:10 -0700

    adding go connection helper

---
+++ rest_golang/mongo/connectionhelper.go
@@ -0,0 +1,45 @@
+package connectionhelper
+
+import (
+	"context"
+	"sync"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+/* Used to create a singleton object of MongoDB client.
+Initialized and exposed through  GetMongoClient().*/
+var clientInstance *mongo.Client
+
+//Used during creation of singleton client object in GetMongoClient().
+var clientInstanceError error
+
+//Used to execute client creation procedure only once.
+var mongoOnce sync.Once
+
+//I have used below constants just to hold required database config's.
+const (
+	CONNECTIONSTRING = "mongodb://localhost:27017"
+)
+
+//GetMongoClient - Return mongodb connection to work with
+func GetMongoClient() (*mongo.Client, error) {
+	//Perform connection creation operation only once.
+	mongoOnce.Do(func() {
+		// Set client options
+		clientOptions := options.Client().ApplyURI(CONNECTIONSTRING)
+		// Connect to MongoDB
+		client, err := mongo.Connect(context.TODO(), clientOptions)
+		if err != nil {
+			clientInstanceError = err
+		}
+		// Check the connection
+		err = client.Ping(context.TODO(), nil)
+		if err != nil {
+			clientInstanceError = err
+		}
+		clientInstance = client
+	})
+	return clientInstance, clientInstanceError
+}
