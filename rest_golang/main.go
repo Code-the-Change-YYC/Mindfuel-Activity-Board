ba0773606ffdd3a15febafd18a03bad6f8a5e0bf
parsahonarmand <parsa.honarmand@gmail.com>
2022-01-18 01:48:06 -0700

    Added retries for socket connection

--- rest_golang/main.go
+++ rest_golang/main.go
@@ -1,18 +1,16 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"flag"
 	"log"
 	"net/url"
-	"os"
-	"os/signal"
 	"time"
 
-	"mindfuel.ca/activity_rest/mongo"
-
-	"github.com/gorilla/websocket"
+	"github.com/recws-org/recws"
 	"mindfuel.ca/activity_rest/model"
+	"mindfuel.ca/activity_rest/mongo"
 )
 
 // var addr = flag.String("addr", "wonderville.org:5556", "http service address")
@@ -21,110 +19,94 @@ var addr = flag.String("addr", "localhost:3210", "http service address")
 var done = make(chan struct{})
 var mock_server_url = "ws://localhost:3210"
 
-func messageHandler(c *websocket.Conn) {
+func messageHandler(message []byte) {
 	mongoClient, err := mongo.GetMongoClient()
 	if err != nil {
 		log.Println("Error creating a mongodb client")
 		return
 	}
-	defer close(done)
-	for {
-		_, message, err := c.ReadMessage()
-		if err != nil {
-			log.Println("Error in recv: ", err)
-			return
-		}
 
-		var msg interface{}
-		err = json.Unmarshal([]byte(message), &msg)
+	var msg interface{}
+	err = json.Unmarshal([]byte(message), &msg)
+	if err != nil {
+		log.Println("Error in unmarshalling json: ", err)
+		return
+	}
+	// cast the message into a map to see what kind of message it is
+	msgMap := (msg).(map[string]interface{})
+
+	var asset model.AssetMessage
+	var session model.SessionMessage
+
+	dateTime := time.Now().UTC()
+	asset.Date = dateTime
+	session.Date = dateTime
+
+	switch msgMap["type"] {
+	case "wondervilleAsset":
+		err = json.Unmarshal([]byte(message), &asset)
 		if err != nil {
 			log.Println("Error in unmarshalling json: ", err)
 			return
 		}
-		// cast the message into a map to see what kind of message it is
-		msgMap := (msg).(map[string]interface{})
-
-		var asset model.AssetMessage
-		var session model.SessionMessage
-
-		dateTime := time.Now().UTC()
-		asset.Date = dateTime
-		session.Date = dateTime
-
-		switch msgMap["type"] {
-		case "wondervilleAsset":
-			err = json.Unmarshal([]byte(message), &asset)
-			if err != nil {
-				log.Println("Error in unmarshalling json: ", err)
-				return
-			}
-			log.Println("Wonderville Asset: ", asset)
-			mongo.InsertAssets(mongoClient, asset)
-		case "wondervilleSession":
-			err = json.Unmarshal([]byte(message), &session)
-			if err != nil {
-				log.Println("Error in unmarshalling json: ", err)
-				return
-			}
-			log.Println("Wonderville Session: ", session)
-			mongo.InsertSessions(mongoClient, session)
-
-			log.Println("Wonderville Session: ", session)
-		default:
-			log.Println("Unrecognized message: ", msg)
+		log.Println("Wonderville Asset: ", asset)
+		mongo.InsertAssets(mongoClient, asset)
+	case "wondervilleSession":
+		err = json.Unmarshal([]byte(message), &session)
+		if err != nil {
+			log.Println("Error in unmarshalling json: ", err)
+			return
 		}
+		log.Println("Wonderville Session: ", session)
+		mongo.InsertSessions(mongoClient, session)
+
+		log.Println("Wonderville Session: ", session)
+	default:
+		log.Println("Unrecognized message: ", msg)
 	}
 }
 
 func main() {
-	flag.Parse()
-	log.SetFlags(0)
-
-	interrupt := make(chan os.Signal, 1)
-	signal.Notify(interrupt, os.Interrupt)
-
 	// if testing locally, comment the below line and
 	// uncomment the one below it
 	// u := url.URL{Scheme: "wss", Host: *addr}
 	u := url.URL{Scheme: "ws", Host: *addr}
-	log.Printf("connecting to %s", u.String())
 
-	c, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
-	if err != nil {
-		log.Fatal("error connecting: ", err)
+	// from https://github.com/recws-org/recws
+	ctx := context.Background()
+	ws := recws.RecConn{
+		KeepAliveTimeout: 0,
+		RecIntvlMin:      10 * time.Second,
+		RecIntvlFactor:   1,
 	}
-	defer c.Close()
-	go messageHandler(c)
+	ws.Dial(u.String(), nil)
 
-	// Not sure if we need all this
-	ticker := time.NewTicker(time.Second)
-	defer ticker.Stop()
-
+	// the program will always be trying to connect every 10 seconds until
+	// the connection is established.
 	for {
 		select {
-		case <-done:
+		case <-ctx.Done():
+			go ws.Close()
+			log.Printf("Websocket closed %s", ws.GetURL())
 			return
-		case t := <-ticker.C:
-			err := c.WriteMessage(websocket.TextMessage, []byte(t.String()))
-			if err != nil {
-				log.Println("write: ", err)
-				return
+		default:
+			if !ws.IsConnected() {
+				time.Sleep(10 * time.Second)
+				log.Printf("Websocket disconnected %s", ws.GetURL())
+				continue
 			}
-		case <-interrupt:
-			log.Println("interrupt")
 
-			// Cleanly close the connection by sending a close message and then
-			// waiting (with timeout) for the server to close the connection.
-			err := c.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
-			if err != nil {
-				log.Println("write close:", err)
+			if err := ws.WriteMessage(1, []byte("Incoming")); err != nil {
+				log.Printf("Error: WriteMessage %s", ws.GetURL())
 				return
 			}
-			select {
-			case <-done:
-			case <-time.After(time.Second):
+
+			_, message, err := ws.ReadMessage()
+			if err != nil {
+				log.Printf("Error: ReadMessage %s", ws.GetURL())
+				continue
 			}
-			return
+			go messageHandler(message)
 		}
 	}
 }
