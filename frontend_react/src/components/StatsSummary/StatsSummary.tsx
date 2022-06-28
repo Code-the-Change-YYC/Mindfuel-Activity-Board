c5697a76d888e0e6b0afff83720b64eaf4299da4
parsahonarmand <parsa.honarmand@gmail.com>
2022-06-27 18:53:55 -0600

    Piechart initial implementation

--- frontend_react/src/components/StatsSummary/StatsSummary.tsx
+++ frontend_react/src/components/StatsSummary/StatsSummary.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
-
-import { CircularProgress, Fade, FormControl, IconButton, InputLabel, MenuItem, Select } from "@material-ui/core";
+import { CircularProgress, Fade, FormControl, 
+  FormControlLabel, FormGroup, IconButton, 
+  InputLabel, MenuItem, Select, Switch } from "@material-ui/core";
 import Modal from "@material-ui/core/Modal";
 import Paper from "@material-ui/core/Paper";
 import { StylesProvider, withStyles } from "@material-ui/core/styles";
@@ -13,6 +14,7 @@ import TableRow from "@material-ui/core/TableRow";
 import { EqualizerOutlined } from "@material-ui/icons";
 import { AxiosResponse } from "axios";
 import { Image } from "react-bootstrap";
+import { PieChart, Pie, Legend, Tooltip, Cell } from 'recharts';
 
 import ApiService from "../../api/ApiService";
 import activityIcon from "../../res/assets/map-marker-activity.svg";
@@ -22,6 +24,7 @@ import videoIcon from "../../res/assets/map-marker-video.svg";
 import { ActivityStatsApiResponse } from "../../utils/ApiServiceInterface";
 import { getTimelineDate, numberFormatter } from "../../utils/helpers";
 import { Stats } from "../../utils/Stats";
+import { ChartStat } from "../../utils/ChartStat";
 import styles from "./StatsSummary.module.css";
 import { useAppDispatch } from "../../state/hooks";
 import { setAlert } from "../../state/actions";
@@ -67,6 +70,8 @@ const StatsSummary = () => {
   const [loading, setLoading] = useState(false);
   const [open, setOpen] = useState(false);
   const [stats, setStats] = useState<Stats[]>([]);
+  const [chartValues, setChartValues] = useState<ChartStat[]>([]);
+  const [chartVisibility, setChartVisibility] = useState<boolean>(false);
   const [trendingVal, setTrendingVal] = useState<number>(items[0].value);
 
   const formClasses = {
@@ -92,6 +97,17 @@ const StatsSummary = () => {
   const tableRowClasses = {
     root: styles.tableRow,
   };
+
+  const colors = ['#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6', 
+  '#E6B333', '#3366E6', '#999966', '#99FF99', '#B34D4D',
+  '#80B300', '#809900', '#E6B3B3', '#6680B3', '#66991A', 
+  '#FF99E6', '#CCFF1A', '#FF1A66', '#E6331A', '#33FFCC',
+  '#66994D', '#B366CC', '#4D8000', '#B33300', '#CC80CC', 
+  '#66664D', '#991AFF', '#E666FF', '#4DB3FF', '#1AB399',
+  '#E666B3', '#33991A', '#CC9999', '#B3B31A', '#00E680', 
+  '#4D8066', '#809980', '#E6FF80', '#1AFF33', '#999933',
+  '#FF3380', '#CCCC00', '#66E64D', '#4D80CC', '#9900B3', 
+  '#E64D66', '#4DB380', '#FF4D4D', '#99E6E6', '#6666FF'];
 
   useEffect(() => {
     // Get initial data on instantiation
@@ -125,11 +141,38 @@ const StatsSummary = () => {
       .then(
         (response: AxiosResponse<ActivityStatsApiResponse>) => {
           setStats(response.data.stats);
+          updateChart()
         },
         () => handleApiError()
       )
       .finally(() => setLoading(false));
   };
+
+  const handleChartVisibility = () => {
+    if (chartVisibility) setChartVisibility(false)
+    else {
+      updateChart()
+      setChartVisibility(true)
+    }
+  }
+
+  const updateChart = () => {
+    let newChart: ChartStat[] = []
+    loop1:
+    for (let i = 0; i < stats.length; i++) {
+      let newStat: ChartStat = {value: 0, name: ""}
+      for (let j = 0; j < newChart.length; j++) {
+        if (newChart[j].name === stats[i].type) {
+          newChart[j].value += stats[i].hits
+          continue loop1;
+        }
+      }
+      newStat.value = stats[i].hits;
+      newStat.name = stats[i].type;
+      newChart.push(newStat)
+    }
+    setChartValues(newChart)
+  }
 
   const getRows = () => {
     if (loading) {
@@ -181,7 +224,10 @@ const StatsSummary = () => {
       >
         <Fade in={open}>
           <div className={styles.modal}>
-            <FormControl classes={formClasses}>
+            <FormControl 
+              classes={formClasses} 
+              style={{display:"flex", flexDirection:"row", alignContent:"space-between"}}
+            >
               <InputLabel classes={inputLabelClasses} style={{ color: "white" }}>
                 Trending
               </InputLabel>
@@ -197,20 +243,50 @@ const StatsSummary = () => {
                   </MenuItem>
                 ))}
               </Select>
+              <FormGroup style={{marginLeft: "auto"}}>
+                <FormControlLabel labelPlacement="top" control={
+                  <Switch onClick={handleChartVisibility}/>
+                } label="Chart" />
+              </FormGroup>
             </FormControl>
-            <TableContainer classes={tableContainerClasses} component={Paper}>
-              <Table classes={tableHeaderClasses} size="small" aria-label="stats table">
-                <TableHead>
-                  <TableRow>
-                    <CustomTableCell>Icon</CustomTableCell>
-                    <CustomTableCell>Category</CustomTableCell>
-                    <CustomTableCell>Top</CustomTableCell>
-                    <CustomTableCell align="right">Sessions</CustomTableCell>
-                  </TableRow>
-                </TableHead>
-                <TableBody>{getRows()}</TableBody>
-              </Table>
-            </TableContainer>
+            {chartVisibility && 
+              <PieChart width={450} height={400}>
+                <Pie
+                  dataKey="value"
+                  isAnimationActive={true}
+                  data={chartValues}
+                  cx={"50%"}
+                  cy={"50%"}
+                  outerRadius={"80%"}
+                  fill="#ffdd00"
+                  stroke='#52247f'
+                  label
+                >
+                  {
+                    chartValues.map((entry, index) => (
+                      <Cell key={`cell-${index}`} fill={colors[index]}/>
+                    ))
+                  }
+                </Pie>
+                <Tooltip />
+                <Legend verticalAlign="bottom" height={36}/>
+              </PieChart>
+            }
+            {!chartVisibility &&
+              <TableContainer classes={tableContainerClasses} component={Paper}>
+                <Table classes={tableHeaderClasses} size="small" aria-label="stats table">
+                  <TableHead>
+                    <TableRow>
+                      <CustomTableCell>Icon</CustomTableCell>
+                      <CustomTableCell>Category</CustomTableCell>
+                      <CustomTableCell>Top</CustomTableCell>
+                      <CustomTableCell align="right">Sessions</CustomTableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>{getRows()}</TableBody>
+                </Table>
+              </TableContainer>
+            }
           </div>
         </Fade>
       </Modal>
---
+++ frontend_react/src/utils/ChartStat.ts
@@ -0,0 +1,5 @@
+export type ChartStat = {
+  name: string;
+  value: number;
+};
+
