748ade49e41d282faaa78ceae2dc4dd7ca411430
Ramin Radmand <69999501+imraminradmand@users.noreply.github.com>
2022-11-08 18:06:03 -0700

    Addressing erroneous error log (#53)

--- rest_golang/socket/listener.go
+++ rest_golang/socket/listener.go
@@ -35,8 +35,9 @@ func messageHandler(mongoClient *mongo.Client, message []byte) {
 			return
 		}
 		db.InsertUser(mongoClient, user)
+		return
 	} else {
-		log.Println(log.Ldate, " Unrecognized message:", msg)
+		log.Println(log.Ldate, " Unrecognized message - User Insertion:", msg)
 	}
 
 	if val, ok := msgMap["type"]; ok && val == model.WondervilleAsset {
@@ -47,7 +48,7 @@ func messageHandler(mongoClient *mongo.Client, message []byte) {
 		}
 		db.InsertActivityStats(mongoClient, user)
 	} else {
-		log.Println(log.Ldate, " Unrecognized message:", msg)
+		log.Println(log.Ldate, " Unrecognized message - ActivityStats Insertion:", msg)
 	}
 }
 
